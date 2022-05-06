09e75e7966f76374dc339ddbb1a926a2adc9e637
Valeria <evejurado4@gmail.com>
2022-05-06 18:09:35 -0500

    cambio

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+<h1 class="animate__animated animate__backInLeft">Login</h1>
+   <form action="signup.php" method="post">
+       <input type="text" name="Nombre (s)" placeholder="Ingrese su nombre">
+       <input type="text" name="Apellido (s)" placeholder="Ingrese su apellido">
+       <input type="text" name="Nombre de usuario" placeholder="Ingrese su nombre de usuario">
+       <input type="password" name="Contraseña" placeholder="Ingrese su constraseña">
+       <input type="password" name="Confirme su contraseña" placeholder="Ingrese su constraseña">
+       <input type="text" name="Empresa a la que pertenece" placeholder="Ingrese la empresa">
+       <input type="text" name="Cargo que desempeña" placeholder="Ingrese su cargo">
+       <input type="text" name="Email" placeholder="Ingrese su email">
+    </form>
+</body>
+</html>
